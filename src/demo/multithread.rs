e2ed65e2c4a68437a7ddd2fbfe331a2353cca786
DrEden33773 <edwardwang33773@gmail.com>
2025-01-21 21:29:01 +0800

    improved test cases

--- src/demo/multithread.rs
+++ src/demo/multithread.rs
@@ -1,11 +1,13 @@
-use alloc::sync::Arc;
+use alloc::{borrow::ToOwned, sync::Arc};
 use spin::mutex::Mutex;
 
 pub async fn mutex() {
   const RES: usize = 3;
 
   let counter = Arc::new(Mutex::<usize>::new(0));
-  for _ in 0..RES {
-    let _ = counter.clone();
+  for i in 0..RES {
+    // let _ = counter.clone();
+    let mut v = Arc::new(Mutex::<usize>::new(i));
+    counter.clone_into(&mut v);
   }
 }
